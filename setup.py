d87fbda501decca9cb14d91509efc77aa883c8e5
AndrewKing <11490628+andrew000@users.noreply.github.com>
2020-08-23 16:57:38 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='machine2human',
+    version='0.0.1',
+    packages=['machine2human'],
+    url='https://github.com/andrew000/machine2human',
+    license='MIT License',
+    author='AndrewKing',
+    author_email='',
+    description='Make machine more friendly to you! Convert seconds to a human string and back!'
+)
